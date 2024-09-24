f71311948dfc724da7b826794c7b7881f0785767
Divyansh Gupta <divyanshgupta.dg123@gmail.com>
2024-09-25 00:17:35 +0530

    nvim multi cursor and hyprland

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -8,6 +8,52 @@ return {
             "saadparwaiz1/cmp_luasnip",
             "rafamadriz/friendly-snippets",
         },
+        build = "make install_jsregexp",
+        config = function()
+            local ls = require("luasnip")
+            local s = ls.snippet
+            local t = ls.text_node
+            local i = ls.insert_node
+            local extras = require("luasnip.extras")
+            local rep = extras.rep
+            local fmt = require("luasnip.extras.fmt").fmt
+            local c = ls.choice_node
+            local f = ls.function_node
+            local d = ls.dynamic_node
+            local sn = ls.snippet_node
+            vim.keymap.set({ "i", "s" }, "<A-n>", function()
+                if ls.choice_active() then
+                    ls.change_choice(1)
+                end
+            end)
+
+            vim.keymap.set({ "i", "s" }, "<A-k>", function()
+                if ls.expand_or_jumpable() then
+                    ls.expand_or_jump()
+                end
+            end, { silent = true })
+
+            vim.keymap.set({ "i", "s" }, "<A-j>", function()
+                if ls.jumpable(-1) then
+                    ls.jump(-1)
+                end
+            end, { silent = true })
+            ls.add_snippets("tex", {
+                s("hello", {
+                    t('print("hello, '),
+                    -- i(1),
+                    i(1, "default"),
+                    t(' world")'),
+                    i(2),
+                    t(' world")'),
+                }),
+            })
+            -- ls.add_snippets("tex", {
+            --     s("hello", {
+            --         t('print("hello, world")'),
+            --     }),
+            -- })
+        end,
     },
     {
         "hrsh7th/nvim-cmp",
